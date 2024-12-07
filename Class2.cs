b9e2e3860359c6a8e376a779ab75f61b3797b3e6
Akash kumar <79831596+Shakya-2001@users.noreply.github.com>
2024-12-07 11:17:54 +0530

    Add files via upload

---
+++ Class2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bankOpen
+{
+    internal class Class2
+    {
+        static void Main(string[] args)
+        {
+            Typeofacc obj=new Typeofacc();
+            obj.GetDetails();
+            obj.Deposite();
+            obj.Withdraw();
+            obj.Display();
+        }
+    }
+}
